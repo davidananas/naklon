d18c9b99dac5fcae6e0df35b60e39cbcab876d5f
davidananas <116786922+davidananas@users.noreply.github.com>
2022-12-24 07:40:55 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
+function vitvor () {
+    tečka.delete()
+    tečka = game.createSprite(2, 2)
+}
+input.onGesture(Gesture.LogoUp, function () {
+    vitvor()
+    tečka.turn(Direction.Right, 90)
+    while (input.isGesture(Gesture.LogoUp)) {
+        tečka.move(1)
+        basic.pause(100)
+    }
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    vitvor()
+    while (input.isGesture(Gesture.TiltLeft)) {
+        tečka.move(-1)
+        basic.pause(100)
+    }
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    vitvor()
+})
 input.onGesture(Gesture.TiltRight, function () {
     vitvor()
     while (input.isGesture(Gesture.TiltRight)) {
@@ -9,28 +31,6 @@ input.onGesture(Gesture.LogoDown, function () {
     vitvor()
     tečka.turn(Direction.Left, 90)
     while (input.isGesture(Gesture.LogoDown)) {
-        tečka.move(1)
-        basic.pause(100)
-    }
-})
-function vitvor () {
-    tečka.delete()
-    tečka = game.createSprite(2, 2)
-}
-input.onGesture(Gesture.TiltLeft, function () {
-    vitvor()
-    while (input.isGesture(Gesture.TiltLeft)) {
-        tečka.move(-1)
-        basic.pause(100)
-    }
-})
-input.onGesture(Gesture.ScreenUp, function () {
-    vitvor()
-})
-input.onGesture(Gesture.LogoUp, function () {
-    vitvor()
-    tečka.turn(Direction.Right, 90)
-    while (input.isGesture(Gesture.LogoUp)) {
         tečka.move(1)
         basic.pause(100)
     }
